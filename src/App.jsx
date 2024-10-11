3e0049a7c3d63e2a0f8dc628b5e507b61f5cd749
Rosvic <rosemaryoguezuonu@gmail.com>
2024-10-11 13:51:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Header from "./header/header";
+import Home from "./home/home";
+import Mode from "./mode/mode";
+import Sidebar from "./header/sidebar";
+import { useState } from "react";
+import { Context } from "./Context";
+
+function App() {
+  const [togglebar, setTogglebar] = useState(false);
+  return (
+    <>
+      <div className="h-[5vh]  ">
+        <Context.Provider value={{ togglebar, setTogglebar }}>
+          <Header />
+          <Mode />
+          <Sidebar />
+        </Context.Provider>
+      </div>
+      <Home />
+    </>
+  );
+}
+
+export default App;
