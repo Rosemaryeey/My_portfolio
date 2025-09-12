06e59e66fc09d41beb4da49ea0850187f8158943
Rosvic <rosemaryoguezuonu@gmail.com>
2025-09-12 13:33:49 +0100

    adjusted footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@
 import "./header.css";
 export default function Footer() {
   return (
-    <footer className="p-4 bg text-center">
-      <p>© {new Date().getFullYear()} My Website. All rights reserved.</p>
+    <footer className="p-6 bg text-center">
+      <p>Rosvic © {new Date().getFullYear()} My Website. All rights reserved.</p>
     </footer>
   );
 }
